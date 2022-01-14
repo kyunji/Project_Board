42c29c609776c9d9b4ec02881ae9bb5ed00e68eb
kyunji <kyunji@ajou.ac.kr>
2022-01-14 16:02:55 +0900

    ADD DB Connection(JPA), data insertion

--- Board/src/main/java/com/example/demo/controller/UserController.java
+++ Board/src/main/java/com/example/demo/controller/UserController.java
@@ -1,9 +1,9 @@
 package com.example.demo.controller;
 
-import com.example.demo.domain.UserDto;
+import com.example.demo.domain.User;
+import com.example.demo.repository.UserRepository;
 
-import java.time.LocalDate;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,18 +11,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+	
+	@Autowired
+    private UserRepository userRepository;
+	
 	@PostMapping("/signup")
-	public String mainPage(Model model,UserDto user){
-		LocalDate now = LocalDate.now();
-
-		model.addAttribute("userId",user.getUserId());
-		model.addAttribute("userName",user.getUserName());
-		model.addAttribute("userPassword",user.getUserPassword());
-		model.addAttribute("userEmail",user.getUserEmail());
-		model.addAttribute("userAddDate",now);
-		System.out.print("======================= \n");
-		System.out.print(user);
-		System.out.print("======================= \n");
+	public String mainPage(Model model,User user){
+		model.addAttribute(user.getEmail());
+		model.addAttribute(user.getName());
+		
+		User user1=new User();
+    	user1.setEmail(user.getEmail());
+    	user1.setName(user.getName());
+        userRepository.save(user1);
+        //userRepository.flush();
+        System.out.println(userRepository.findAll());
 		
 		return "index";
 	}
