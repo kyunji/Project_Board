cc57d6f23b06e02be9fe15ea9632fad5b3d45760
kyunji <kyunji@ajou.ac.kr>
2022-01-19 17:46:17 +0900

    ADD board-front(vue)

---
+++ Board/src/test/java/com/example/demo/UserRepositoryTest.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.domain.User;
+import com.example.demo.repository.UserRepository;
+
+@SpringBootTest
+//@Transactional
+class UserRepositoryTest {
+	
+    @Autowired
+    private UserRepository userRepository;
+    
+    @Test
+    void crud() { // create, read, update, delete
+    	User user=new User();
+    	user.setEmail("david@fastcampus.com");
+    	user.setName("david");
+        userRepository.save(user);
+        //userRepository.flush();
+        System.out.println(userRepository.findAll());
+
+    }
+}
