e52b5d48b317c64f022e060f6c469751f908b337
kyunji <kyunji@ajou.ac.kr>
2022-01-20 16:55:12 +0900

    ADD signup page

--- Board/src/main/java/com/board/controller/UserController.java
+++ Board/src/main/java/com/board/controller/UserController.java
@@ -10,15 +10,25 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
+//@CrossOrigin(origins="http://localhost:8081")
 @RestController // JSON 형태의 객체 return
 //@NoArgsConstructor //생성자 주입 방법
 public class UserController {
-    @Autowired
+    @Autowired //필드주입
     private UserService userService;
+
+    //welcome page
+    @RequestMapping("/")
+    public String home(){
+        System.out.println("home \n");
+        return "home";
+    }
+
     //회원가입
-    @PutMapping("/signup")
+    @PutMapping("/api/signup")
     public void signupUser(@RequestBody UserDTO requestDto) {
         userService.signupService(requestDto);
+        System.out.println("signup \n");
     }
 }
 
