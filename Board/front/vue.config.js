e76e80f9f706606def244eedd101951c2747b4ae
kyunji <kyunji@ajou.ac.kr>
2022-01-21 14:56:19 +0900

    remove .git

---
+++ Board/front/vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {  
+    outputDir: "../src/main/resources/static",  
+    indexPath: "../static/index.html",  
+    devServer: {
+      proxy:{
+        '/': {
+          "target": 'http://localhost:8080',
+          //"pathRewrite": { '^/': '' },
+          "changeOrigin": true,
+          
+      }
+      }
+    },  
+    chainWebpack: config => {  
+      const svgRule = config.module.rule("svg");    
+      svgRule.uses.clear();    
+      svgRule.use("vue-svg-loader").loader("vue-svg-loader");  
+    }  
+  };
