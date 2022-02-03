603b7e69c684835dec94f5c864dc31f41c6f9a23
kyunji <kyunji@ajou.ac.kr>
2022-02-03 15:26:41 +0900

    ADD Front v-text-field validation check rules

--- Board/src/App.vue
+++ Board/src/App.vue
@@ -1,60 +1,88 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
+    <v-card
+      color="grey lighten-4"
+      flat
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+      <v-app-bar
+        color="green darken-2"
+        dark
+        dense
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+        <v-app-bar-nav-icon @click="drawer = !drawer" />
+        <v-toolbar-title> Blog </v-toolbar-title>
+        <v-spacer />
+      </v-app-bar>
+    </v-card>
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-icon>mdi-account</v-icon>
+        </v-list-item-avatar>
 
+        <v-list-item-content>
+          <v-list-item-title>User</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider />
+
+      <v-list dense>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+          :to="item.to"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-main>
-      <HelloWorld/>
+    <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      drawer: false,
+      items: [
+        {
+          title: 'Home',
+          icon: 'mdi-home',
+          to: '/'
+        },
+        {
+          title: 'Signup',
+          icon: 'mdi-account',
+          to: '/signup'
+        },
+        {
+          title: 'Login',
+          icon: 'mdi-lock',
+          to: '/login'
+        },
+        {
+          title: 'Board',
+          icon: 'mdi-pencil',
+          to: '/board'
+        },
+      ]
+    }
   },
-
-  data: () => ({
-    //
-  }),
-};
+}
 </script>
---
+++ Board/src/components/Blog.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-card
+    height="800"
+    width="256"
+    class="mx-auto"
+  >
+    <v-navigation-drawer permanent>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-card>
+</template>
+  <!--
+  <div id="nav">
+    <h1>Hello App!</h1>
+    <section>
+      <router-link to="/hello">hello</router-link> |
+      <router-link to="/test">test</router-link>
+    </section>
+    <router-view></router-view>
+  </div>
+  -->
+
+<script>
+  export default {
+    data () {
+      return {
+        items: [
+          { title: 'SignUp', icon: 'mdi-view-dashboard' },
+          { title: 'Login', icon: 'mdi-image' },
+          { title: 'About', icon: 'mdi-help-box' },
+        ],
+        right: null,
+      }
+    },
+  }
+</script>
