4608f1262d21fedef2465e32abbd798b0cc085d3
kyunji <kyunji@ajou.ac.kr>
2022-02-03 16:47:11 +0900

    ADD front Board write form

--- Board/front/src/components/Board.vue
+++ Board/front/src/components/Board.vue
@@ -1,26 +1,68 @@
 <template>
-  <div id="app">
-    <!-- 글 제목 작성 칸 -->
-    <p> title:
-      <input type="text" v-model="title" id="title"> 
-    </p>
-    <!-- 글 내용 작성 칸 -->
-    <textarea
-    rows=20
-    cols=100
+  <v-app>
+  <v-main>
+  <v-card >
+    <v-form
+      ref="form"
+      v-model="form"
+      class="pa-4 pt-6"
     >
-    </textarea>
-  </div>
+    <h2 class="text-center"> Write </h2> 
+      <v-text-field
+        v-model="title"
+        filled
+        color="green darken-4"
+        counter="100"
+        label="Title"
+        style="min-height: 80px"
+      
+      ></v-text-field>
+        <v-textarea
+          filled
+          name="input-7-4"
+          label="Contents"
+          value="내용을 입력하세요"
+        ></v-textarea>
+      
+    </v-form>
+
+    <v-divider></v-divider>
+
+    <v-card-actions>
+      <v-btn
+        text
+        @click="$refs.form.reset()"
+      >
+        Clear
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn
+        :disabled="!form"
+        class="white--text"
+        color="green darken-4"
+        depressed
+        @click.prevent="onSignUp"
+      >
+        Save
+      </v-btn>
+    </v-card-actions>
+    <v-dialog
+      v-model="dialog"
+      absolute
+      max-width="400"
+      persistent
+    >
+    </v-dialog>
+  </v-card>
+  </v-main>
+  
+</v-app>
+
+
 </template>
 
+
 <script>
-export default {
-  name: 'app'
-}
+ 
 </script>
 
-<style>
-#app {
-  margin: 20px;
-}
-</style>
