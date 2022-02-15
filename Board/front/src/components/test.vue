cb5631c22ae1a911fbfe9b42df37a979de024da4
kyunji <kyunji@ajou.ac.kr>
2022-02-15 09:40:07 +0900

    DELETE front(vue), test

--- Board/front/src/components/test.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <v-app id="app">
-    <v-btn color="success">Success</v-btn>
-    <v-btn color="error">Error</v-btn>
-    <v-btn color="warning">Warning</v-btn>
-    <v-btn color="info">Info</v-btn>
-  </v-app>
-</template>
